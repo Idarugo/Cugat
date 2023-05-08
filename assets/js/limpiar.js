e950d81a0e551ada84c6ae54ff4f5efe86464bdc
idarugo <onepiecemonkeydluffy20@gmail.com>
2023-05-08 12:02:20 -0400

    0.4

--- assets/js/limpiar.js
+++ assets/js/limpiar.js
@@ -1,5 +1,6 @@
 document.getElementById("btn-limpiar").addEventListener("click", function() {
     document.getElementById("select-local").value = "";
     document.getElementById("input-codigo").value = "";
+    document.getElementById("titulo").value = "";
   });
   
